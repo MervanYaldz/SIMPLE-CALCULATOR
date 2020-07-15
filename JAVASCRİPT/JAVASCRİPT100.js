8e0302d058e8a9bb1ccffbcbe7da5c93701b5388
shakespeare34 <syald0216@gmail.com>
2020-07-15 17:32:41 +0300

    calculator

---
+++ JAVASCRİPT/JAVASCRİPT100.js
@@ -0,0 +1,24 @@
+function topla(){
+	a=parseInt(document.FORM1.number1.value);
+	b=parseInt(document.FORM1.number2.value);
+	c=a+b;
+	document.FORM1.result=c;
+}
+function cıkar(){
+	a=parseInt(document.FORM1.number1.value);
+	b=parseInt(document.FORM1.number2.value);
+	c=a-b;
+	document.FORM1.result=c;
+}
+function carp(){
+	a=parseInt(document.FORM1.number1.value);
+	b=parseInt(document.FORM1.number2.value);
+	c=a*b;
+	document.FORM1.result=c;
+}
+function bol(){
+	a=parseInt(document.FORM1.number1.value);
+	b=parseInt(document.FORM1.number2.value);
+	c=a/b;
+	document.FORM1.result=c;
+}
